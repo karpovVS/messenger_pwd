cd7ddc6b28687bdf50aa02a493b903fe96d77418
karpovVS <kazankarpov@gmail.com>
2019-12-22 14:59:28 +0300

    Add user and messages tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Stalin", password:"fossacat")
+User.create(username:"Fossa", password:"fossacat")
+User.create(username:"Fobos", password:"fossacat")
+User.create(username:"Lulia", password:"fossacat")
+User.create(username:"Wolfik", password:"fossacat")
+User.create(username:"Basya", password:"fossacat")
+User.create(username:"Pawuk", password:"fossacat")
