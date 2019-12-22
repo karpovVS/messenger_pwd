f70b9fbd224246cfc40d0cfab93eb5f27e14576f
karpovVS <kazankarpov@gmail.com>
2019-12-22 20:42:33 +0300

    Add chatroom chanel conroller | ActionCable| CoffeScript

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 post 'message', to: 'messages#create'
+
+mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
